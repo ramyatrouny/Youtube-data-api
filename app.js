3416afe384d87cd1eb715edd322a669ceebe1bfd
rami.atrouni <ramiatrouni@gmail.com>
2021-03-11 21:03:51 +0200

    separate auth from api

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
+const verifyMiddleware = require('./middleware/verifyMiddleware');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const indexRouter = require('./routes/api');
-const connectDB = require('./config/db');
+const authRouter = require('./routes/auth');
 
 app.use(morgan('dev'));
 
@@ -18,10 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/documentation', (req, res) => {
+app.use('/documentation', (_, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.use('/api', indexRouter);
+// Public routes that do not require a bearer token
+app.use('/auth', authRouter);
+// Routes that are restricted for logged in users
+// Handled through verifyMiddleware
+app.use('/api', verifyMiddleware, indexRouter);
 
 module.exports = app;
