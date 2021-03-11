dfe665818bd3ddf44535f42e14736a7f2c2820c3
rami.atrouni <ramiatrouni@gmail.com>
2021-03-11 14:58:29 +0200

    User Model

---
+++ models/UserModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userModel = mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    fullName: {
+        type: String,
+        require: true,
+    },
+    youtube_channel_id: {
+        type: String,
+        require: true,
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userModel);
