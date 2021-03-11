53f6783a5fca665def89e4feffb90c5db6b095d4
rami.atrouni <ramiatrouni@gmail.com>
2021-03-11 14:40:03 +0200

    Add ApiDoc files

---
+++ public/api_data.js
@@ -0,0 +1,202 @@
+define({ "api": [
+  {
+    "type": " post ",
+    "url": "/auth/login",
+    "title": "User Login",
+    "name": "login",
+    "description": "<p>Authenticate a user</p>",
+    "group": "Everyone",
+    "permission": [
+      {
+        "name": "isPublic"
+      }
+    ],
+    "version": "0.0.0",
+    "filename": "controller/user/login.js",
+    "groupTitle": "Everyone",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "email",
+            "description": "<p>user email address</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "password",
+            "description": "<p>user password</p>"
+          }
+        ]
+      }
+    }
+  },
+  {
+    "type": " post ",
+    "url": "/auth/register",
+    "title": "User signup",
+    "name": "register",
+    "description": "<p>Register a new user</p>",
+    "group": "Everyone",
+    "permission": [
+      {
+        "name": "isPublic"
+      }
+    ],
+    "version": "0.0.0",
+    "filename": "controller/user/signup.js",
+    "groupTitle": "Everyone",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "email",
+            "description": "<p>user email address</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "password",
+            "description": "<p>user password</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "full_name",
+            "description": "<p>user full name</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "youtube_channel_id",
+            "description": "<p>user youtube channel id</p>"
+          }
+        ]
+      }
+    }
+  },
+  {
+    "type": " get ",
+    "url": "/auth/me",
+    "title": "Logged in User Details",
+    "name": "Personal_Details",
+    "description": "<p>Get the current user details</p>",
+    "group": "User",
+    "permission": [
+      {
+        "name": "isAuthenticated"
+      }
+    ],
+    "version": "0.0.0",
+    "filename": "controller/user/self.js",
+    "groupTitle": "User"
+  },
+  {
+    "type": " post ",
+    "url": "/auth/refresh",
+    "title": "Refresh user access token",
+    "name": "refreshToken",
+    "description": "<p>Refresh user token</p>",
+    "group": "User",
+    "permission": [
+      {
+        "name": "isAuthenticated"
+      }
+    ],
+    "version": "0.0.0",
+    "filename": "controller/user/refreshToken.js",
+    "groupTitle": "User"
+  },
+  {
+    "type": " put ",
+    "url": "/auth/:id/update",
+    "title": "Update logged in user information",
+    "name": "updateUserDetails",
+    "description": "<p>update user details</p>",
+    "group": "User",
+    "permission": [
+      {
+        "name": "isAuthenticated"
+      }
+    ],
+    "version": "0.0.0",
+    "filename": "controller/user/updateUser.js",
+    "groupTitle": "User",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "email",
+            "description": "<p>user email address</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "password",
+            "description": "<p>user password</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "full_name",
+            "description": "<p>user full name</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "youtube_channel_id",
+            "description": "<p>user youtube channel id</p>"
+          }
+        ]
+      }
+    }
+  },
+  {
+    "type": " get ",
+    "url": "/videos/:id",
+    "title": "get the videos details",
+    "name": "userVideoDetails",
+    "description": "<p>get the current video details</p>",
+    "group": "Video",
+    "permission": [
+      {
+        "name": "isAuthenticated"
+      }
+    ],
+    "version": "0.0.0",
+    "filename": "controller/videos/videoDetails.js",
+    "groupTitle": "Video"
+  },
+  {
+    "type": " get ",
+    "url": "/videos",
+    "title": "get the user videos",
+    "name": "userVideos",
+    "description": "<p>get the user videos</p>",
+    "group": "Video",
+    "permission": [
+      {
+        "name": "isAuthenticated"
+      }
+    ],
+    "version": "0.0.0",
+    "filename": "controller/videos/userVideos.js",
+    "groupTitle": "Video"
+  }
+] });
---
+++ public/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "360 Mea Challenge",
+  "version": "1.0.0",
+  "description": "360MEA API documentation submitted by Rami Atrouni",
+  "author": "Rami Atrouni",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-03-11T12:39:47.851Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
