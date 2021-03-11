8f0a0e1366ebb637d0e59e4fb727df6e1ac7ac7a
rami.atrouni <ramiatrouni@gmail.com>
2021-03-11 20:56:36 +0200

    add error codes

---
+++ util/errorCode.js
@@ -0,0 +1,6 @@
+module.exports = {
+    validationError: 'VALIDATION_ERROR',
+    databaseError: 'DATABASE_ERROR',
+    unexpectedError: 'UNEXPECTED_ERROR',
+    unauthenticated: 'UNAUTHENTICATED'
+};
