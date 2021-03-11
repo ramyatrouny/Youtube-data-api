5f33cdd81e16221b5a0ad03462b79e6dd320bd12
rami.atrouni <ramiatrouni@gmail.com>
2021-03-11 14:58:16 +0200

    controller to connect mongodb

--- config/db.js
+++ config/db.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
+const logger = require('../config/logger');
+
 module.exports = async () => {
     try {
+        logger.info(`Trying to connect to Mongodb`);
+
+        const connection = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+
+        logger.info(`MongoDB connected: ${connection.connection.host}`);
 
     } catch (error) {
-
+        logger.error(`Unable to connect to MongoDB`);
+        logger.error(`Error: ${process.env.message}`);
+        process.exit(1);
     }
 }
