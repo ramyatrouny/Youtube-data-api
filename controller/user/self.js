43346a4d31c33b2632ed6733fc88d8765b26b5de
rami.atrouni <ramiatrouni@gmail.com>
2021-03-12 12:44:33 +0200

    create route to return current user information

--- controller/user/self.js
+++ controller/user/self.js
@@ -6,6 +6,45 @@
  * @apiPermission isAuthenticated
  */
 
-module.exports = (req, res) => {
 
+const fileName = __filename.split(/(\\|\/)/g).pop();
+const logger = require('../../config/logger');
+const UserModel = require('../../model/UserModel');
+const errorCode = require('../../util/errorCode');
+
+module.exports = async (req, res) => {
+	try {
+
+		logger.info(`${fileName} Checking if the user ${req.user} exists`);
+		const user = await UserModel.findById(req.user).select('-__v -createdAt -updatedAt -password');
+
+		if (!user) {
+			logger.warn(`${fileName} User does not exists`);
+
+			// Security note: To avoid data leak it's better not to say if the user exists or not
+			return res.status(400).json({
+				status: 'error',
+				data: {
+					code: errorCode.validationError,
+					message: `User does not exist`
+				}
+			})
+		}
+
+		return res.status(200).json({
+			status: 'success',
+			data: user
+		})
+
+
+	} catch (error) {
+		logger.error(`${fileName} Internal Server error ${error}`)
+		return res.status(500).json({
+			status: 'error',
+			data: {
+				code: errorCode.unexpectedError,
+				message: 'Internal server error'
+			}
+		})
+	}
 }
